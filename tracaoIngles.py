738f2d3f1149ce2d1551e5135af6e094ecfb05fa
MatheusRantin <130771700+MatheusRantin@users.noreply.github.com>
2023-11-06 18:19:05 -0300

    Add files via upload

---
+++ tracaoIngles.py
@@ -0,0 +1,54 @@
+from translate import Translator
+import customtkinter as ctk
+
+
+
+
+
+
+class TelaHome:
+
+    def traduzir(self):
+        tradutor = Translator(to_lang="pt")
+        getTexto = str(self.inputTexto.get())
+
+        #Limite de 500 Char
+        saida = tradutor.translate(str(getTexto))
+
+        with open("arquivo_Traduzido.txt", "a") as arquivo:
+            arquivo.write(f"Tradução:{saida}.")
+        print(f"Traducao: {saida}")
+
+
+    def __init__ (self, master):
+        self.master = master
+
+        self.LabelTitulo = ctk.CTkLabel(master, text= "Tradutor de texto")
+        self.LabelTitulo.pack(padx=10, pady=10)
+
+        self.inputTexto = ctk.CTkEntry(master, placeholder_text="Digite o texto")
+        self.inputTexto.pack(padx=10, pady=10)
+
+        self.btnSubmit = ctk.CTkButton(master, text="Traduzir", command= self.traduzir )
+        self.btnSubmit.pack(padx=10, pady=10)
+        
+
+        
+
+
+
+if __name__ == "__main__":
+    root = ctk.CTk()
+    root.title("Tradução de Texto")
+    root.geometry("500x500")
+
+    chamandoTela = TelaHome(root)
+
+    root.mainloop()
+
+
+
+   
+
+
+
